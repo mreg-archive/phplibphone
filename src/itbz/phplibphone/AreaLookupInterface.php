8d495d2e8685479bf11f6401f546bd6e92c91bea
itbz <hannes.forsgard@gmail.com>
2012-06-05 11:41:55 +0200

    Lookup libraries now handles their own country codes

---
+++ src/itbz/phplibphone/AreaLookupInterface.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * This file is part of the phplibphone package
+ *
+ * Copyright (c) 2012 Hannes Forsgård
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Hannes Forsgård <hannes.forsgard@gmail.com>
+ *
+ * @package phplibphone
+ */
+namespace itbz\phplibphone;
+
+
+/**
+ * Interface for area code lookup libraries
+ *
+ * @package phplibphone
+ */
+interface AreaLookupInterface extends LookupInterface
+{
+
+    /**
+     * Get country code this library handles
+     *
+     * @return int
+     */
+    public function getCountryCode();
+
+}
